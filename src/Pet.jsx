79fc5742f1140e1129d38e4a01864b26f73eb93f
Carlos Matheu Armengol <info@plastikaweb.com>
2024-11-15 15:42:39 +0100

    add routes

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,10 +1,22 @@
-const Pet = props => {
+import { Link } from 'react-router-dom';
+
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = 'http://pets-images.dev-apis.com/pets/none.jpg';
+
+  if (images.length) {
+    hero = images[0];
+  }
+
   return (
-    <div>
-      <h2>{props.name}</h2>
-      <h3>{props.animal}</h3>
-      <h3>{props.breed}</h3>
-    </div>
+    <Link to={`/details/${id}`} className='pet'>
+      <div className='image-container'>
+        <img src={hero} alt={name} />
+      </div>
+      <div className='info'>
+        <h2>{name}</h2>
+        <h3>{`${animal} — ${breed} — ${location}`}</h3>
+      </div>
+    </Link>
   );
 };
 
---
+++ src/Results.jsx
@@ -0,0 +1,25 @@
+import Pet from './Pet';
+
+const Results = ({ pets }) => {
+  return (
+    <div className='search'>
+      {!pets.length ? (
+        <h1>No Pets Found</h1>
+      ) : (
+        pets.map(pet => (
+          <Pet
+            id={pet.id}
+            key={pet.id}
+            animal={pet.animal}
+            name={pet.name}
+            breed={pet.breed}
+            images={pet.images}
+            location={`${pet.city}, ${pet.state}`}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Results;
